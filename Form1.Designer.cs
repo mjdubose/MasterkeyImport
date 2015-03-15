857d1f80148047b6ebe4359d7606ff2d4be581b0
Matthew DuBose <matthew.dubose@gmail.com>
2015-03-15 07:26:23 -0400

    First commit
    Thingee that does nifty stuff to things

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace MasterkeyImport
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dg_Convert = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.convertToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.convertCSVToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenCSVDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SaveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dg_Convert)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dg_Convert
+            // 
+            this.dg_Convert.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg_Convert.Location = new System.Drawing.Point(66, 85);
+            this.dg_Convert.Name = "dg_Convert";
+            this.dg_Convert.Size = new System.Drawing.Size(896, 336);
+            this.dg_Convert.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.convertToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1026, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.closeToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(37, 20);
+            this.toolStripMenuItem1.Text = "File";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.openFileToolStripMenuItem.Text = "Open File";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(121, 6);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            // 
+            // convertToolStripMenuItem
+            // 
+            this.convertToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.convertCSVToolStripMenuItem});
+            this.convertToolStripMenuItem.Name = "convertToolStripMenuItem";
+            this.convertToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.convertToolStripMenuItem.Text = "Convert";
+            // 
+            // convertCSVToolStripMenuItem
+            // 
+            this.convertCSVToolStripMenuItem.Name = "convertCSVToolStripMenuItem";
+            this.convertCSVToolStripMenuItem.Size = new System.Drawing.Size(140, 22);
+            this.convertCSVToolStripMenuItem.Text = "Convert CSV";
+            this.convertCSVToolStripMenuItem.Click += new System.EventHandler(this.convertCSVToolStripMenuItem_Click);
+            // 
+            // OpenCSVDialog
+            // 
+            this.OpenCSVDialog.FileName = "\"\"";
+            this.OpenCSVDialog.Filter = "CSV files|*.csv|All FIles|*.*";
+            this.OpenCSVDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.OpenCSVDialog_FileOk);
+            // 
+            // SaveFileDialog
+            // 
+            this.SaveFileDialog.Filter = "CSV files|*.csv|All FIles|*.*";
+            this.SaveFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1026, 455);
+            this.Controls.Add(this.dg_Convert);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Master Key System CSV Converter";
+            ((System.ComponentModel.ISupportInitialize)(this.dg_Convert)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dg_Convert;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem convertToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem convertCSVToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog OpenCSVDialog;
+        private System.Windows.Forms.SaveFileDialog SaveFileDialog;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace MasterkeyImport
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            var result = OpenCSVDialog.ShowDialog();
+            if (result != DialogResult.OK) return;
+            var fileinfo = new FileInfo(OpenCSVDialog.FileName);
+
+
+            using (var filestr = new FileStream(fileinfo.DirectoryName + "\\schema.ini",
+                FileMode.Create, FileAccess.Write))
+            {
+                using (var writer = new StreamWriter(filestr))
+                {
+                    //  1                      2        3        4                5              6        7       9       9     10  11  12  13  14  15  16  17  18  19  20  21  22  23  24  25  26   27   28     29 30
+                    //"HPCSoft MasterKing","3/13/2015","Page 1","arooga test - 2","Key Symbol","Bitting","Qty","Comments","AB1","6","1","5","0","5","7","6"," "," "," "," "," "," "," "," "," ", " ", " ","0.00"," ",""
+                    writer.WriteLine("[" + fileinfo.Name + "]");
+                    writer.WriteLine("ColNameHeader=False");
+                    writer.WriteLine("Format=CSVDelimited");
+                    writer.WriteLine("DateTimeFormat=MM-dd-yy");
+                    writer.WriteLine("Col1=Generated_By Text");
+                    writer.WriteLine("Col2=Date Text");
+                    writer.WriteLine("Col3=Page_Number Text");
+                    writer.WriteLine("Col4=Keying_System_Name Text");
+                    writer.WriteLine("Col5=Key_Symbol Text");
+                    writer.WriteLine("Col6=Complete_Bitting Text");
+                    writer.WriteLine("Col7=Qty Text");
+                    writer.WriteLine("Col8=Comments Text");
+                    writer.WriteLine("Col9=Complete_Key_Symbol Text");
+                    writer.WriteLine("Col10=Bitting_1 Text");
+                    writer.WriteLine("Col11=Bitting_2 Text");
+                    writer.WriteLine("Col12=Bitting_3 Text");
+                    writer.WriteLine("Col13=Bitting_4 Text");
+                    writer.WriteLine("Col14=Bitting_5 Text");
+                    writer.WriteLine("Col15=Bitting_6 Text");
+                    writer.WriteLine("Col16=Bitting_7 Text");
+                    writer.WriteLine("Col17=R Text");
+                    writer.WriteLine("Col18=S Text");
+                    writer.WriteLine("Col19=T Text");
+                    writer.WriteLine("Col20=U Text");
+                    writer.WriteLine("Col21=V Text");
+                    writer.WriteLine("Col22=W Text");
+                    writer.WriteLine("Col23=X Text");
+                    writer.WriteLine("Col24=Y Text");
+                    writer.WriteLine("Col25=Z Text");
+                    writer.WriteLine("Col26=AA Text");
+                    writer.WriteLine("Col27=AB Text");
+                    writer.WriteLine("Col28=AC Text");
+                    writer.WriteLine("Col29=AD Text");
+                    writer.WriteLine("Col30=AE Text");
+                    writer.Close();
+                    writer.Dispose();
+                }
+                filestr.Close();
+                filestr.Dispose();
+            }
+
+
+
+            string file = Path.GetFileName(OpenCSVDialog.FileName);
+            string dir = Path.GetDirectoryName(OpenCSVDialog.FileName);
+
+            string cStr = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + dir + ";"
+                          + "Extended Properties='Text;HDR=No;FMT=Delimited';";
+            string sqlStr = "SELECT * FROM [" + file + "]";
+            var dt = new DataTable();
+            try
+            {
+                var da = new OleDbDataAdapter(sqlStr, cStr);
+                da.Fill(dt);
+            }
+            catch { dt = null; }
+
+
+            dg_Convert.DataSource = dt;
+        }
+        private static void ExportDgvtoCsv(DataGridView dgv,FileSystemInfo fileInfo)
+        {
+            if (dgv.Columns.Count == 0) return;
+            var myString = new StringBuilder();
+
+            if (File.Exists(fileInfo.FullName))
+            {
+                File.Delete(fileInfo.FullName);
+            }
+
+            using (var sw = new StreamWriter(fileInfo.FullName, true))
+            {
+
+                // loop through each row of our DataGridView
+
+                for (int i = 0; i < dgv.Rows.Count - 1; i++)
+                {
+                    var row = dgv.Rows[i];
+                    for (int index = 0; index < row.Cells.Count; index++)
+                    {
+
+
+                        var cell = row.Cells[index];
+
+                        myString.Append("\"" + cell.Value + "\"");
+                        if (index != row.Cells.Count - 1)
+                        {
+                            myString.Append(",");
+                        }
+                    }
+                    myString.Append(Environment.NewLine);
+                }
+
+                sw.Write(myString.ToString());
+               
+            }
+
+            GC.Collect();
+        }
+        private void convertCSVToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var result = SaveFileDialog.ShowDialog();
+            if (result != DialogResult.OK) return;
+            var fileinfo = new FileInfo(SaveFileDialog.FileName);
+
+            ExportDgvtoCsv(dg_Convert, fileinfo);
+        }
+
+        private void saveFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void OpenCSVDialog_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+       
+    }
+}
